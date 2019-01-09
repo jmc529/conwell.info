1e111da3f36443ec8368a46dd8f8d9403d583d04
jmc529 <jmc529@vt.edu>
2019-01-09 00:50:26 -0500

    added more structure, added resume.pdf

--- js/script.js
+++ js/script.js
@@ -1,11 +1,22 @@
 $(document).ready(function() {
 
-  // Check for click events on the navbar burger icon
-  $(".navbar-burger").click(function() {
+    // Check for click events on the navbar burger icon
+    $(".navbar-burger").click(function() {
 
       // Toggle the "is-active" class on both the "navbar-burger" and the "navbar-menu"
       $(".navbar-burger").toggleClass("is-active");
       $(".navbar-menu").toggleClass("is-active");
 
-  });
+    });
+
+    var prevScrollpos = $(window).pageYOffset;
+    $(window).on('scroll', function(){
+      var currentScrollPos = window.pageYOffset;
+      if (prevScrollpos > currentScrollPos) {
+        $("#navbar")[0].style.top = "0";
+      } else {
+        $("#navbar")[0].style.top = "-50px";
+      }
+      prevScrollpos = currentScrollPos;
+    });
 });
